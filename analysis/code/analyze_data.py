b4cb4f85f6a5bd5fb3abd04ff2cb56f1558255f2
xzmerry <zimeixia@uchicago.edu>
2022-08-18 16:00:06 -0500

    #3 Add regression for subset >= 1960

--- analysis/code/analyze_data.py
+++ analysis/code/analyze_data.py
@@ -5,12 +5,28 @@ from linearmodels import PanelOLS
 ### DEFINE
 def main():
     df = import_data()
-    fit = run_regression(df)
-    formatted = format_model(fit)
+    fit1 = run_regression(df)
+    formatted1 = format_model(fit1)
+
+    fit2 = run_regression_1960(df)
+    formatted2 = format_model(fit2)
     
-    with open('output/regression.csv', 'w') as f:
+    write_csv(formatted1, 'regression1.csv')
+    write_csv(formatted2, 'regression2.csv')
+
+    # with open('output/regression.csv', 'w') as f:
+    #     f.write('<tab:regression>' + '\n')
+    #     formatted1.to_csv(f, sep = '\t', index = False, header = False)
+    #     formatted2.to_csv(f, sep = '\t', index = False, header = False)
+
+    
+
+def write_csv(formatted, name = ''):
+    file_name = 'output/'+ name
+    with open(file_name, 'w') as f:
         f.write('<tab:regression>' + '\n')
         formatted.to_csv(f, sep = '\t', index = False, header = False)
+
     
 def import_data():
     df = pd.read_csv('input/data_cleaned.csv')
@@ -20,6 +36,15 @@ def import_data():
 
 def run_regression(df):
     df = df.set_index(['county_id', 'year'])
+    model = PanelOLS.from_formula('chips_sold ~ 1 + post_tv + EntityEffects + TimeEffects', data = df)
+    fit = model.fit()
+    
+    return(fit)
+
+def run_regression_1960(df):
+    df = df.set_index(['county_id', 'year'])
+    # add code to run only on the subset of Yr>= 1960
+    df =  df.iloc[df.index.get_level_values('year') >= 1960]
     model = PanelOLS.from_formula('chips_sold ~ 1 + post_tv + EntityEffects + TimeEffects', data = df)
     fit = model.fit()
     
